963c1b407ceb8b4c6c75cb68d4807488da9526b7
Payakorn Saksuriya <payakornn@gmail.com>
2022-12-25 16:34:29 +0700

    add test chack number of loaded data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,30 @@
 using VRPTW
 using Test
 
-@testset "VRPTW.jl" begin
-    # Write your tests here.
-    @test 1 == 1
+
+# load solomon data for 25
+@testset "VRPTW load solomon data 25" begin
+    num_node = 25
+    instance = load_solomon_data("r101", num_node=num_node)
+    @test instance.num_node == num_node
+    @test length(instance.service_time) == num_node + 1
+    @test length(instance.demand) == num_node + 1
+end
+
+
+# load solomon data for 50
+@testset "VRPTW load solomon data 25" begin
+    num_node = 50
+    instance = load_solomon_data("r101", num_node=num_node)
+    @test instance.num_node == num_node
+    @test length(instance.service_time) == num_node + 1
+    @test length(instance.demand) == num_node + 1
+end
+
+# load solomon data for 100
+@testset "VRPTW load solomon data 100" begin
+    instance = load_solomon_data("r101")
+    @test instance.num_node == 100
+    @test length(instance.service_time) == 101
+    @test length(instance.demand) == 101
 end
