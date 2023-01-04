222ac956899ebb45b29210034ae61e6e4a04b580
Payakorn Saksuriya <payakornn@gmail.com>
2023-01-04 15:13:46 +0700

    add point

--- src/VRPTW.jl
+++ src/VRPTW.jl
@@ -1,5 +1,6 @@
 module VRPTW
 
+import Base.push!, Base.splice!
 using Revise, JLD2
 
 # Write your package code here.
@@ -8,6 +9,6 @@ include("load_data.jl")
 include("solution.jl")
 
 # export function that clould be used
-export load_solomon_data, dir, dir_data, Solution, Problem, swap!, add!
+export load_solomon_data, dir, dir_data, Solution, Problem, swap!, add!, push!, splice!, empty_solution, fix_route_zero, route_length
 
 end
